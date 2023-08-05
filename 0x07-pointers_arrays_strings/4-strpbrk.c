0d0ec0c440efe9c3363acc8bfa6ace50dab0d79a
shehabali55 <shehab@example.com>
2023-08-04 20:00:27 -0700

    done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include <stddef.h>
-/**
- *_strpbrk- searches a string for any of a set of bytes.
- *@s:The null-terminated string to be scanned
- *@accept:he null-terminated string containing the characters to match.
- *
- *Return:Returns a pointer to the byte in s that matches
- * one of the bytes in accept, or NULL if no such byte is found
- */
 
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: string to be scanned
+ * @accept: string containing characters to match
+ * Description: This function finds the first character in the string s
+ * that matches any character specified in accept.
+ * This does not include terminating null-characters.
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	char *start = accept;
+	int i, j;
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (*accept)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*accept == *s)
-				return (s);
-			accept++;
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
 		}
-		accept = start;
-		s++;
 	}
+
 	return (0);
 }
