212896c72184b042c1b4efe3f8d7237889d0119d
shehabali55 <shehab@example.com>
2023-08-09 16:23:18 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[])
+/**
+ * main Write a program that prints its name, followed by a new line.
+ **/
+int main (int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n",argv[0]);
